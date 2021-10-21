d2cce9a23b598f283a28c08affa09ac0f9c19b79
lostfields <nicolai.gjaerum@gmail.com>
2021-10-21 10:51:26 +0200

    persistent using localStorage

--- src/app.js
+++ src/app.js
@@ -11,12 +11,11 @@ export default {
         return {
             authorizationUrl: 'https://auth.dev.tfso.io/authorize',
             tokenUrl: 'https://auth.dev.tfso.io/oauth2/token',
-            clientId: 'w00uUIgCyeCsIpzUcWqAIbqzh05YR9LA',
-            clientSecret: '_qUvBiVaxU7gwB2A0m_uLGSsZAMAvFw9B0MXY7LqSM_ZliFlDscwNJzc0skt3iWb',
-            scope: 'openid offline_access organization',
-            audience: 'https://app.24sevenoffice.com',
-            tokenKeys: 'https://auth.dev.tfso.io/.well-known/jwks.json',
-            license: '810957928405876',
+            clientId: window.localStorage.getItem(`tfso:auth-playground:clientId`) ?? '***',
+            clientSecret: window.sessionStorage.getItem(`tfso:auth-playground:clientSecret`) ?? '***',
+            scope: window.localStorage.getItem(`tfso:auth-playground:scope`) ?? 'openid offline_access organization',
+            audience: window.localStorage.getItem(`tfso:auth-playground:audience`) ?? 'https://app.24sevenoffice.com',
+            license: window.localStorage.getItem('`tfso:auth-playground:license`') ?? '810957928405876',
             mode: 'Implicit'
         };
     },
@@ -25,8 +24,19 @@ export default {
             this.mode = mode
         },
         onEdit(key, value) {
-            if(key in this.$data)
+            if(key in this.$data) {
+                switch(key) {
+                    case 'clientSecret':
+                        window.sessionStorage.setItem(`tfso:auth-playground:${key}`, value)
+                        break
+
+                    default:
+                        window.localStorage.setItem(`tfso:auth-playground:${key}`, value)
+                        break
+                }
+
                 this.$data[key] = value
+            }
         }
     },
     render() {
